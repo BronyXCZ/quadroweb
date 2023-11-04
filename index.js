c4554c20c5fe43a4470573bc9bede32023961c01
jonybrozek@gmail.com <jonybrozek@gmail.com>
2023-11-04 22:53:23 +0100

    a

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
 const app = require('express')();
 const port = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-    res.send("Hello world!");
-})
-
-app.listen(port, () => {
-    console.log("App up at port " + port)
+    document.body.innerHTML = fs.readFileSync(path.join(__dirname, "sites/home.html"))
 })
