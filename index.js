b3d4b5d50dfe7fc99f01ed7d1ac31055277f74de
jonybrozek@gmail.com <jonybrozek@gmail.com>
2023-11-05 09:06:51 +0100

    index

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    
     fs.readFile('./sites/home.html', 'utf8', (err, data) => {
         if (err) {
             res.statusCode = 500;
@@ -13,6 +12,10 @@ const server = http.createServer((req, res) => {
             console.error('Error:', err);
             res.end('Experienced error whilst loading an HTML');
         } else {
+            fs.readFile('./css/home.css', 'utf8', (err, data2) => {
+                console.log(data2);
+            })
+
             res.statusCode = 200;
             res.setHeader('Content-Type', 'text/html');
             res.end(data);
