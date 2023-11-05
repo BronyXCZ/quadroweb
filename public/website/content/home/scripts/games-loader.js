ed73633dbfabe76925465b9fbefc4e55bd35009b
jonybrozek@gmail.com <jonybrozek@gmail.com>
2023-11-05 16:37:03 +0100

    commit

--- public/website/content/home/scripts/games-loader.js
+++ public/website/content/home/scripts/games-loader.js
@@ -11,7 +11,8 @@ async function projects_update() {
     })
     if (group_data) {
         let group_gamedata = []
-        let game_getmedia = async function(game_data) {
+        for (i = 0; i < group_data.data.length; i++) {
+            let game_data = group_data.data[i]
             let thumbnail_data = await new Promise((resolve) => {
                 fetch(proxy_address + 'https://games.roblox.com/v2/games/' + game_data.id + '/media')
                 .then(response => {
@@ -25,10 +26,9 @@ async function projects_update() {
                         resolve(response.json())
                      })
                 })
-                group_gamedata[group_gamedata.length] = {id: game_data.id, url: thumbnail_address.data[0].imageUrl}
+                group_gamedata[group_gamedata.length] = {id: game_data.rootPlace.id, url: thumbnail_address.data[0].imageUrl}
             }
         }
-        group_data.data.forEach((game_data) => game_getmedia(game_data))
 
         let element_prefab_html = await new Promise((resolve) => {
             fetch(proxy_address + 'http://quadrostudios.xyz/website/content/home/prefabs/ourgamesdiv-game.html')
@@ -36,26 +36,35 @@ async function projects_update() {
                 resolve(response.text())
             })
         })
-        console.log(element_prefab_html)
 
         let games_elements = document.getElementsByClassName('ourgamesdiv-game')
-        games_elements.forEach((element, index) => {
+        for (let index = games_elements.length - 1; index >= 0; index--) {
+            let element = games_elements[index]
             let group_game = group_gamedata[index]
             if (group_game) {
-                element.childNodes[0].href = "https://roblox.com/" + group_game.id
-                element.childNodes[1].src = group_game.url
+                element.getElementsByClassName('ourgamesdiv-game-link')[0].href = "https://roblox.com/games/" + group_game.id + "/"
+                element.getElementsByClassName('ourgamesdiv-game-img')[0].src = group_game.url
             } else {
                 element.remove()
             }
-        })
+        }
         group_gamedata.forEach((game_data, index) => {
             if (!games_elements[index]) {
                 let element_new = document.createElement('div')
+                document.getElementsByClassName('ourgamessubdiv')[0].appendChild(element_new)
                 element_new.className = "ourgamesdiv-game"
-                element_new.innerHTML = fs.readFile('./prefabs/ourgamesdiv-game.html')
+                element_new.innerHTML = element_prefab_html
+                element_new.getElementsByClassName('ourgamesdiv-game-link')[0].href = "https://roblox.com/games/" + game_data.id + "/"
+                element_new.getElementsByClassName('ourgamesdiv-game-img')[0].src = game_data.url
             }
         })
     }
 }
 
+function loop() {
+    setTimeout(function() {
+        projects_update()
+        loop()
+    }, 10*1000)
+}
 projects_update()
