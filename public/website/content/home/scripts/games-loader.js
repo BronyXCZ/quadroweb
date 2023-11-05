d0968dd59a6aedd5d2516e93730373cbcb05d2f9
jonybrozek@gmail.com <jonybrozek@gmail.com>
2023-11-05 14:58:05 +0100

    commit

---
+++ public/website/content/home/scripts/games-loader.js
@@ -0,0 +1,34 @@
+// Constants
+const proxy = 'https://qsp-9bf3216ba15c.herokuapp.com/';
+
+// Functions
+async function projects_update() {
+    let group_data = await new Promise((resolve) => {
+        fetch(proxy + 'https://games.roblox.com/v2/groups/9145179/games?accessFilter=1&limit=10&sortOrder=Desc')
+        .then(response => {
+            resolve(response.json());
+        })
+    })
+    console.log(group_data);
+    if (group_data) {
+        group_data.data.forEach((game_data) => async function() {
+            let thumbnail_data = await new Promise((resolve) => {
+                fetch(proxy + 'https://games.roblox.com/v2/games/' + game_data.id + '/media')
+                .then(response => {
+                    resolve(response.json());
+                })
+            })
+            if (thumbnail_data) {
+                let thumbnail_address = await new Promise((resolve) => {
+                     fetch(proxy + 'https://thumbnails.roblox.com/v1/games/' + game_data.id + '/thumbnails?thumbnailIds=' + thumbnail_data.data[0].imageId + '&size=768x432&format=Png&isCircular=false')
+                     .then(response => {
+                        resolve(response.json());
+                     })
+                })
+                console.log(thumbnail_address);
+            }
+        })
+    }
+}
+
+projects_update()
