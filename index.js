600d12c3db4daae3c3549ef5e37d5c514a8348c5
jonybrozek@gmail.com <jonybrozek@gmail.com>
2023-11-05 09:35:20 +0100

    commit

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
-const express = require('express');
+const http = require('http');
+const fs = require('fs');
 const path = require('path');
-const app = express();
-exports.app = app;
 
 const PORT = process.env.PORT || 3000;
-exports.PORT = PORT;
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './public/index.html';
+  }
 
-// Set up a route for the home page
-app.get('/', (req, res) => {
-  console.log("get request");
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.mp4': 'video/mp4',
+  };
+
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.readFile('./public/404.html', (err, content) => {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+        });
+      } else {
+        res.writeHead(500);
+        res.end('Server Error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
