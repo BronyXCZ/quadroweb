98ffb26deb91c1395b3a846f892fd9e73aa35f08
jonybrozek@gmail.com <jonybrozek@gmail.com>
2023-11-05 08:38:45 +0100

    a

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 
-fs.readFile('./sites/home.html', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-        document.body.innerHTML = data;
-    }
-})
+if (typeof document !== undefined) {
+    fs.readFile('./sites/home.html', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            document.body.innerHTML = data;
+        }
+    })
+} else {console.log("Not running on web")}
