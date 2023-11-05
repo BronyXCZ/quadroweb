b3544ea6eb8d7f473934beeff386b527fe17f592
jonybrozek@gmail.com <jonybrozek@gmail.com>
2023-11-05 08:57:40 +0100

    modified

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const app = require('express')();
+const http = require('http');
 const fs = require('fs');
 
-app.get('', (req, res) => {
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    
     fs.readFile('./sites/home.html', 'utf8', (err, data) => {
         if (err) {
-            console.error(err);
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/plain');
+            console.error('Error:', err);
+            res.end('Experienced error whilst loading an HTML');
         } else {
-            res.send(data);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
         }
     })
-})
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
