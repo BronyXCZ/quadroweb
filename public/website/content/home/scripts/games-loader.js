2beea78fd15de328f2e3b8baee83ce592e2e32f8
jonybrozek@gmail.com <jonybrozek@gmail.com>
2023-11-05 15:01:53 +0100

    commit

--- public/website/content/home/scripts/games-loader.js
+++ public/website/content/home/scripts/games-loader.js
@@ -1,10 +1,10 @@
 // Constants
-const proxy = 'https://qsp-9bf3216ba15c.herokuapp.com/';
+const proxy_address = 'https://qsp-9bf3216ba15c.herokuapp.com/';
 
 // Functions
 async function projects_update() {
     let group_data = await new Promise((resolve) => {
-        fetch(proxy + 'https://games.roblox.com/v2/groups/9145179/games?accessFilter=1&limit=10&sortOrder=Desc')
+        fetch(proxy_address + 'https://games.roblox.com/v2/groups/9145179/games?accessFilter=1&limit=10&sortOrder=Desc')
         .then(response => {
             resolve(response.json());
         })
@@ -13,14 +13,14 @@ async function projects_update() {
     if (group_data) {
         group_data.data.forEach((game_data) => async function() {
             let thumbnail_data = await new Promise((resolve) => {
-                fetch(proxy + 'https://games.roblox.com/v2/games/' + game_data.id + '/media')
+                fetch(proxy_address + 'https://games.roblox.com/v2/games/' + game_data.id + '/media')
                 .then(response => {
                     resolve(response.json());
                 })
             })
             if (thumbnail_data) {
                 let thumbnail_address = await new Promise((resolve) => {
-                     fetch(proxy + 'https://thumbnails.roblox.com/v1/games/' + game_data.id + '/thumbnails?thumbnailIds=' + thumbnail_data.data[0].imageId + '&size=768x432&format=Png&isCircular=false')
+                     fetch(proxy_address + 'https://thumbnails.roblox.com/v1/games/' + game_data.id + '/thumbnails?thumbnailIds=' + thumbnail_data.data[0].imageId + '&size=768x432&format=Png&isCircular=false')
                      .then(response => {
                         resolve(response.json());
                      })
