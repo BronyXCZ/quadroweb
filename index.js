6219c3fb3252b3e035cfe282ef23e27dc36bf13d
jonybrozek@gmail.com <jonybrozek@gmail.com>
2023-11-05 08:04:20 +0100

    index

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require('express')();
 const port = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-    document.getElementById("content").innerHTML = fetch('./sites/home.html').text();
+    res.send(fetch('./sites/home.html').text());
 })
